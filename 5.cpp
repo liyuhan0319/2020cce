b6a236180e4a426edbcda5f3e14a42f190ff702f
liyuhan0319 <79677016+liyuhan0319@users.noreply.github.com>
2021-03-26 10:49:38 +0800

    Add files via upload

---
+++ 5.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char line[10]="majority";
+    char line2[10]="ask";
+    if(strcmp(line,line2)>0){
+        printf("left ¤j\n");
+    }else{
+        printf("right ¤j\n");
+    }
+
+}
